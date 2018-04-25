4d9e3497c93bd58ed287d2612eb8ad9542f172bb
Michael <coderliq@gmail.com>
2018-04-25 17:11:11 +0500

    Создание первичной структуры бд

---
+++ isudns/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using isudns.Models;
+using isudns.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace isudns.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IEnumerable<string> GetConfs()
+        {
+            
+            foreach(var c in _context.Conferentions
+                .Include(c => c.Location))
+            {
+                yield return c.Location.Name;
+            }
+
+
+        }
+
+        
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
