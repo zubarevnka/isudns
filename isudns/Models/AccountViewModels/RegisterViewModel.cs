8358639e58d55dd14129b150ed1899869c0daf7b
zubarevnka <zubarevnka@mail.ru>
2018-05-13 17:25:29 +0500

    изменения в Models

--- isudns/Models/AccountViewModels/RegisterViewModel.cs
+++ isudns/Models/AccountViewModels/RegisterViewModel.cs
@@ -15,11 +15,11 @@ namespace isudns.Models.AccountViewModels
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-        //[DataType(DataType.Password)]
+        [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
-        //[DataType(DataType.Password)]
+        [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
