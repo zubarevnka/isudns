545a411430db2dea48e19e27c0e4924e3e00da0d
Michael <coderliq@gmail.com>
2018-05-16 22:25:54 +0500

    Альфа версия

---
+++ isudns/Controllers/ConferentionsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using isudns.Data;
+using isudns.Models;
+
+namespace isudns.Controllers
+{
+    public class ConferentionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ConferentionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Conferentions
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Conferentions.ToListAsync());
+        }
+
+        // GET: Conferentions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var conferention = await _context.Conferentions
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (conferention == null)
+            {
+                return NotFound();
+            }
+
+            return View(conferention);
+        }
+
+        // GET: Conferentions/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Conferentions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Date,Location")] Conferention conferention)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(conferention);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(conferention);
+        }
+
+        // GET: Conferentions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var conferention = await _context.Conferentions.SingleOrDefaultAsync(m => m.Id == id);
+            if (conferention == null)
+            {
+                return NotFound();
+            }
+            return View(conferention);
+        }
+
+        // POST: Conferentions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Date,Location")] Conferention conferention)
+        {
+            if (id != conferention.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(conferention);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ConferentionExists(conferention.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(conferention);
+        }
+
+        // GET: Conferentions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var conferention = await _context.Conferentions
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (conferention == null)
+            {
+                return NotFound();
+            }
+
+            return View(conferention);
+        }
+
+        // POST: Conferentions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var conferention = await _context.Conferentions.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Conferentions.Remove(conferention);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ConferentionExists(int id)
+        {
+            return _context.Conferentions.Any(e => e.Id == id);
+        }
+    }
+}
--- isudns/Program.cs
+++ isudns/Program.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using isudns.Models;
+using isudns.Services;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace isudns
@@ -14,7 +18,30 @@ namespace isudns
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            // BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+            using (var scope = host.Services.CreateScope())
+            {
+                Task t;
+                var services = scope.ServiceProvider;
+                try
+                {
+                    
+
+                    
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                    var rolesManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    t = RoleInitializer.InitializeAsync(userManager, rolesManager);
+                    t.Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
